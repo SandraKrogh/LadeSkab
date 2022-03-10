59071a0150a9e382d560dc2d8650d3b55ef9d6ba
Sandra Krogh <Sandrask85@hotmail.com>
2022-03-10 13:44:01 +0100

    Door + Test flyttet

---
+++ LadeSkab/Door.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeSkab
+{
+    public class Door : IDoor
+    {
+    }
+
+    public event EventHandler<DoorEventArgs> CurrentValueEvent;
+
+    public bool CurrentValue { get; private set; }
+
+    public bool Connected { get; private set; }
+
+    public Door()
+    {
+        CurrentValue = 0.0;
+        Connected = true;
+        _overload = false;
+
+        _timer = new System.Timers.Timer();
+        _timer.Enabled = false;
+        _timer.Interval = CurrentTickInterval;
+        _timer.Elapsed += TimerOnElapsed;
+    }
+
+    private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+    {
+        // Only execute if charging
+        if (_charging)
+        {
+            _ticksSinceStart++;
+            if (Connected && !_overload)
+            {
+                double newValue = MaxCurrent -
+                                  _ticksSinceStart * (MaxCurrent - FullyChargedCurrent) / (ChargeTimeMinutes * 60 * 1000 / CurrentTickInterval);
+                CurrentValue = Math.Max(newValue, FullyChargedCurrent);
+            }
+            else if (Connected && _overload)
+            {
+                CurrentValue = OverloadCurrent;
+            }
+            else if (!Connected)
+            {
+                CurrentValue = 0.0;
+            }
+
+            OnNewCurrent();
+        }
+    }
+
+    public void SimulateConnected(bool connected)
+    {
+        Connected = connected;
+    }
+
+    public void SimulateOverload(bool overload)
+    {
+        _overload = overload;
+    }
+
+    public void StartCharge()
+    {
+        // Ignore if already charging
+        if (!_charging)
+        {
+            if (Connected && !_overload)
+            {
+                CurrentValue = 500;
+            }
+            else if (Connected && _overload)
+            {
+                CurrentValue = OverloadCurrent;
+            }
+            else if (!Connected)
+            {
+                CurrentValue = 0.0;
+            }
+
+            OnNewCurrent();
+            _ticksSinceStart = 0;
+
+            _charging = true;
+
+            _timer.Start();
+        }
+    }
+
+    public void StopCharge()
+    {
+        _timer.Stop();
+
+        CurrentValue = 0.0;
+        OnNewCurrent();
+
+        _charging = false;
+    }
+
+    private void OnNewCurrent()
+    {
+        CurrentValueEvent?.Invoke(this, new CurrentEventArgs() { Current = this.CurrentValue });
+    }
+
+}
